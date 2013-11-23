4e583967ad7da30900feb2bf099a5810444619d5
Andrew Kelly <akelly@8thlight.com>
2013-11-23 17:12:32 -0600

    retrieving singular file with query string

--- scripts/oauthService.js
+++ scripts/oauthService.js
@@ -4,7 +4,15 @@ function scopes() {
 
 var clientId = '21687497772-rbmpab2j0clffvh6lc4o9d94tjrefi3a.apps.googleusercontent.com';
 var apiKey = 'AIzaSyDh8vE4X3m4G4bnlX7FtjgUTnxoywRpidM';
-var scopes = "https://www.googleapis.com/auth/plus.me";
+var scopes = [
+              'https://www.googleapis.com/auth/drive',
+              'https://www.googleapis.com/auth/drive.file',
+              'https://www.googleapis.com/auth/userinfo.profile'
+ ];
+
+function joinedScopes() {
+  return scopes.join(' ');
+}
 
 function handleClientLoad() {
   gapi.client.setApiKey(apiKey);
@@ -12,35 +20,38 @@ function handleClientLoad() {
 }
 
 function checkAuth() {
-  gapi.auth.authorize({'client_id': clientId, 'scope': scopes, 'immediate': true}, handleAuthResult);
+  gapi.auth.authorize({'client_id': clientId, 'scope': joinedScopes(), 'immediate': true}, handleAuthResult);
 }
 
 function handleAuthResult(authResult) {
-  var authorizeButton = $('#authorize-button');
-  authorizeButton.onclick = handleAuthClick;
-
   if (authResult) {
     makeApiCall();
   } else {
     gapi.auth.authorize(
-     {'client_id': clientId, 'scope': scopes, 'immediate': false}, handleAuthResult);
+     {'client_id': clientId, 'scope': joinedScopes(), 'immediate': false}, handleAuthResult);
   }
 }
 
-function handleAuthClick(event) {
-  console.log("clicked");
-  gapi.auth.authorize(
-     {'client_id': clientId, 'scope': scopes, 'immediate': false}, handleAuthResult);
-}
-
 function makeApiCall() {
+
+  gapi.client.load('drive', 'v2', function() {
+    var fileList = gapi.client.drive.files.list({
+      'userId': 'me',
+      'q': "title contains 'tea'"
+    });
+
+    fileList.execute(function(response) {
+      console.log(response)
+    });
+  });
+
   gapi.client.load('plus', 'v1', function() {
     var request = gapi.client.plus.people.get({
       'userId': 'me'
     });
-    request.execute(function(resp) {
-      $('#response').append("<img src=" + resp.image.url + ">");
-      $('#response').append(resp.displayName);
+    request.execute(function(response) {
+      $('#response').append("<img src=" + response.image.url + ">");
+      $('#response').append(response.displayName);
 
     });
   });
