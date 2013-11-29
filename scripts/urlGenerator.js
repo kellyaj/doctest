06fc1ed1d913fa970d97e062d249b5ea6726dd0a
Andrew Kelly <akelly@8thlight.com>
2013-11-29 15:18:51 -0600

    uses proper url to get json

--- scripts/urlGenerator.js
+++ scripts/urlGenerator.js
@@ -2,5 +2,5 @@ var UrlGenerator = function() {
 }
 
 UrlGenerator.jsonUrl = function(key) {
-  return "https://spreadsheets.google.com/feeds/list/" + key + "/1/public/basic?alt=json-in-script"
+  return "https://spreadsheets.google.com/feeds/list/" + key + "/1/public/basic?alt=json"
 }
