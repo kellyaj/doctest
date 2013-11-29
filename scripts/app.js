117b084cb3120426c95135a88c6c9210ee3b8111
Andrew Kelly <akelly@8thlight.com>
2013-11-29 16:29:40 -0600

    retrieving json data and logging it

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   var key = "0AjTtPpTUvG_cdGZZV2lpODFHYzVqOG13WEFUQXJFcXc";
+  var ckey = "0AjTtPpTUvG_cdDR4NGJBeTlJVFFuVFBERF9OUFc2dkE";
+
   var urlGenerator = new UrlGenerator();
+  var rowParser = new RowParser();
 
   var url = UrlGenerator.jsonUrl(key);
+  var entries = []
+
 
   function retrieveData(url, callback) {
     $.getJSON(url).success(function(data) {
-      callback(data);
-    })
+      entries = callback(data.feed.entry);
+    }).complete(function() {
+      console.log(entries);
+    });
   }
-  // pass some callback that will just prune out entries, to be passed for object creation
+
+  retrieveData(url, rowParser.createEntryArray);
 
 });
 
