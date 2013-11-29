15d0593228b1537c9205405a73ed468b4e29410d
Andrew Kelly <akelly@8thlight.com>
2013-11-29 15:44:54 -0600

    makes the json request

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+  var key = "0AjTtPpTUvG_cdGZZV2lpODFHYzVqOG13WEFUQXJFcXc";
+  var urlGenerator = new UrlGenerator();
+
+  var url = UrlGenerator.jsonUrl(key);
+
+  function retrieveData(url, callback) {
+    $.getJSON(url).success(function(data) {
+      callback(data);
+    })
+  }
+  // pass some callback that will just prune out entries, to be passed for object creation
+
 });
 
