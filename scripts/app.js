5a748ab16c6aae2e3fb1dffac62362601f7eede2
Andrew Kelly <kellyaj@gmail.com>
2013-12-03 23:04:06 -0600

    POC for key arrays

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  var key = "0AjTtPpTUvG_cdGZZV2lpODFHYzVqOG13WEFUQXJFcXc";
-  var ckey = "0AjTtPpTUvG_cdDR4NGJBeTlJVFFuVFBERF9OUFc2dkE";
+  var keys = ["0AjTtPpTUvG_cdGZZV2lpODFHYzVqOG13WEFUQXJFcXc", "0AjTtPpTUvG_cdDR4NGJBeTlJVFFuVFBERF9OUFc2dkE"];
 
-  var sheet = new Spreadsheet(key);
-  sheet.retrieve();
-  var sheet2 = new Spreadsheet(ckey);
-  sheet2.retrieve();
+  _.each(keys, function(key) {
+    var sheet = new Spreadsheet(key);
+    sheet.retrieve();
+  });
+
 });
 
