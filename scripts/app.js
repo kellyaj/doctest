b75047e8bfc4ecc2d4ce77c42bf93752cf52960f
Andrew Kelly <akelly@8thlight.com>
2013-12-06 16:39:50 -0600

    adds event for return being hit

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,31 @@ $(document).ready(function() {
   var otherKey = "0AjTtPpTUvG_cdG53SGUwZ3FJcl9VVjV2MWJVQlNUNkE";
 
   $('[data-id=input-button]').click( function() {
-    var key = $('[data-id=key-input]').val();
-    keys.push(key);
+    addKey();
     renderSheets();
   });
 
-  var renderSheets = function () {
+  $('[data-id=key-input]').keyup( function(e) {
+    if (enterWasPressed(e) && inputIsNotEmpty() ) {
+      addKey();
+      renderSheets();
+    }
+  });
+
+  var addKey = function() {
+    keys.push($('[data-id=key-input]').val());
+    $('[data-id=key-input]').val("");
+  }
+
+  var inputIsNotEmpty = function() {
+    return ($('[data-id=key-input]').val() != "");
+  }
+
+  var enterWasPressed = function(e) {
+    return (e.which == 13);
+  }
+
+  var renderSheets = function() {
     _.each(keys, function(key) {
       var sheet = new Spreadsheet(key);
       sheet.retrieve();
